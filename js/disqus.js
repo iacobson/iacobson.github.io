3be5fff01ecc8a2eb25f8334593ee20e49d926cc
iacobson <dorian.iacobescu@gmail.com>
2017-02-12 02:20:38 +0000

    update layout and disqus

---
+++ js/disqus.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+  var disqus_shortname = $('#disqus_thread').data('disqus-shortname');
+  
+
+  (function() {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+  })();
+});
